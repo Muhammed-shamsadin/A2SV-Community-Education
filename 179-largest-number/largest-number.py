e3422afbda92404b8383a37240c7bc1e8ab19614
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-04-30 08:51:02 +0200

    Time: 42 ms (54.37%) | Memory: 16.5 MB (76.21%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,15 +1,15 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        for i, n in enumerate(nums):
-            nums[i] = str(n)
+        for i, n in enumerate(nums):  # i -> index & n -> value
+            nums[i] = str(n)         # change the values to str
 
-        def compare(a, b):
-            if a + b > b + a:
-                return -1
+        def compare(a, b):          # custom compare function to use it in the sorted function as key function
+            if a + b > b + a:       # 330 or 303
+                return -1           # return the 1st one == a
             else:
-                return 1
+                return 1           # return the 2nd one == b
 
-        nums = sorted(nums, key=cmp_to_key(compare))
+        nums = sorted(nums, key=cmp_to_key(compare))  #sorts it in the nums list.
 
         return str(int("".join(nums))) # When [0, 0, 0] output should be => only 0 not "000"
 
