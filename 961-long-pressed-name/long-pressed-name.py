47af95cec23c4f4105233de1ca6b98572cd90e1b
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-08-24 13:17:50 +0300

    Time: 34 ms (70.76%) | Memory: 16.6 MB (45.24%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.py
+++ 961-long-pressed-name/long-pressed-name.py
@@ -1,27 +1,17 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        # If the length of name is greater than typed return False
-        if len(name) > len(typed):
-            return False
-        # If name and typed have the same character, just increment the left and right pointers.
-        left = right = 0
-        while left < len(name):
-            # If the right index is greater than length of typed, return False 
-            if right >= len(typed):
-                return False
-            if name[left] == typed[right]:
+        name_length = len(name)
+        typed_length = len(typed)
+        left = 0
+        right = 0
+
+        while left <= name_length and right < typed_length:
+            if left < name_length and name[left] == typed[right]:
                 left += 1
                 right += 1
-            # If there is a difference, check if the current typed character is the same as the previous charactor of name
+            elif name[left - 1] == typed[right] and left != 0:
+                right += 1
             else:
-                if left != 0 and typed[right] == name[left - 1]:
-                    right += 1
-                else:
-                    return False
-        # If there are any remaining typed characters, check if they are the same as the last character of name 
-        while right < len(typed):
-            if name[left - 1] != typed[right]:
                 return False
-            right += 1
-        return True
         
+        return left == name_length and right == typed_length
