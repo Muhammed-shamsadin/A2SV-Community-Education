4fe0ab536cf8689daac44ebff620d607f443ec27
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-09-28 21:12:36 +0300

    Time: 235 ms (20.55%) | Memory: 16.7 MB (34.25%) - LeetSync

---
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+        length = len(nums)
+        ans = 0
+        for i in range(length):
+            temp = nums[i]
+            for j in range(i, length):
+                temp = math.gcd(temp, nums[j])
+                if temp == k:
+                    ans += 1
+                
+        return ans
