92c4b00bd8b95806a6bc4d136ebf933a63ebde66
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-10-04 23:26:00 +0300

    Time: 45 ms (81.64%) | Memory: 17.3 MB (14.44%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,6 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         length = len(nums)
+
         low = 0
         high = length - 1
         ans = length
@@ -9,9 +10,10 @@ class Solution:
             mid = (low + high) // 2
 
             if nums[mid] >= target:
-                ans = mid
                 high = mid - 1
+                ans = mid
             else:
                 low = mid + 1
-        
+
         return ans
+                
