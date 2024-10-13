9e86cbd3270df4c1547f1a451d269cb65bf82d3e
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-10-13 10:15:07 +0300

    Time: 49 ms (17.24%) | Memory: 16.8 MB (76.26%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         length = len(nums)
-
         low = 0
         high = length - 1
 
@@ -11,17 +10,22 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            # Left sorted
-            if nums[low] <= nums[mid]:
+            if nums[low] == nums[mid] and nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+                continue    
+
+        # Left-half is sorted 
+            elif nums[low] <= nums[mid]:
                 if nums[low] <= target and target <= nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
-            # Right sorted
+        # Right-half is sorted
             else:
                 if nums[mid] <= target and target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid - 1
-        
+
         return -1
