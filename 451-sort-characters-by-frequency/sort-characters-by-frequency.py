a3c9898052b989ce57c06545f00387a00e28dbfa
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-10-14 10:55:16 +0300

    Time: 42 ms (77.11%) | Memory: 17.9 MB (59.39%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,39 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        '''
+        Approach: first hold the numbers of the frequency ..and then we will sort alphabetically.
+        '''
+        # l = [i for i in s]
+        # l = sorted(l,key=lambda x: l.count(x))
+        # print(l)
+        # l.reverse()
+        # return "".join(l)
+
+        counter = Counter(s)
+        # bucket = defaultdict(list)
+        # for key, val in counter.items():
+        #     bucket[val].append(key)
+        # # print(bucket)
+
+        # res = []
+        # for i in range(len(s), 0, -1):
+        #     # print(bucket[i])
+        #     for c in bucket[i]:
+        #         # print(c)
+        #         res.append(c * i)
+        #         # print(res)
+
+        oj = []
+
+        for key, val in counter.items():
+            oj.append([val, key])
+        
+        print(oj)
+
+        oj.sort(reverse=True)
+        res = []
+        for bezat, char in oj:
+            res.append(bezat * char)
+
+        return "".join(res)
+
