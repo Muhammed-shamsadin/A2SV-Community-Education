2cc86d645cd2a6d4609a578f684a2e1cbeebd3c3
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-10-16 12:27:30 +0300

    Time: 73 ms (96.48%) | Memory: 17.5 MB (83.87%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        '''
+        Approach: We keep the count of the black balls and if a white ball appears after the black ball 
+                  that means we need to swap the black ball with the white ball.
+                  More of it is about counting the possibilities of swapping.
+        '''
+
+        swap, black = 0, 0
+
+        for char in s:
+            if char == "0":
+                swap += black
+            else:
+                black += 1
+        
+        return swap
