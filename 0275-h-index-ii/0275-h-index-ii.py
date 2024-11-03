566c1dfea0064169942f6ddab96edca082204bf0
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-11-03 10:19:06 +0300

    Time: 4 ms (14.71%), Space: 22 MB (94.12%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        max_citation = max(citations)
-        counts = [0] * (max_citation + 1)
-        
-        # Step 1: Count the number of papers for each citation count
-        for citation in citations:
-            counts[citation] += 1
-        
-        total_papers = 0
-        
-        # Step 2: Calculate how many papers have at least `h` citations
-        for h in range(max_citation, -1, -1):
-            total_papers += counts[h]
-            if total_papers >= h:
-                return h
-        
-        return 0  # Fallback
+        length = len(citations)
+        low = 0
+        high = length - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if citations[mid] == length - mid:
+                return length - mid
+            elif citations[mid] < length - mid:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return length - low
+
+            
