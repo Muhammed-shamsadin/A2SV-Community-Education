25608cda4cc836f74072e831119afb7021771d86
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-11-30 15:47:31 +0300

    Time: 57 ms (41.40%) | Memory: 18.3 MB (7.39%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.py
@@ -0,0 +1,77 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index >= self.size or index < 0:
+            return -1
+        current = self.head
+        for _ in range(0, index):
+            current = current.next
+        return current.val
+        
+
+    def addAtHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size:
+            return
+        current = self.head
+        new_node = ListNode(val)
+
+        if index <= 0:
+            new_node.next = current
+            self.head = new_node
+        else:
+            for _ in range(index - 1):
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+        
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+
+        current = self.head
+
+        if index == 0:
+            self.head = self.head.next
+        else:
+            for _ in range(0, index - 1):
+                current = current.next
+            current.next = current.next.next
+
+        self.size -= 1
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
