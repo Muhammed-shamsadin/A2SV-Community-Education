1f00fe6758202a2beee1bad352aba361c7ca285f
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-12-03 19:54:31 +0300

    Time: 1 ms (34.61%) | Memory: 17.3 MB (6.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        '''
+        Approach: we only have those characters in the string => '(', ')', '{', '}', '[' and ']'
+        '''
         stack = []
+
         for char in s:
-            if not stack:
-                stack.append(char)
-            elif (stack[-1] == '(' and char == ')') or (stack[-1] == '{' and char == '}') or (stack[-1] == '[' and char == ']'):
-                stack.pop()
+            if stack:
+                # Check if the top of the stack matches the closing bracket
+                if (stack[-1] == '(' and char == ')') or (stack[-1] == '{' and char == '}') or (stack[-1] == '[' and char == ']'):
+                    stack.pop()
+                else:
+                    stack.append(char)
             else:
                 stack.append(char)
-        
+
         if stack:
             return False
         return True
-        
