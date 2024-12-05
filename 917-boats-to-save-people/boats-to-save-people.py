a91d80a8ef7dee0ccc92bcffa9b0e5b26b24c337
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-12-05 21:00:34 +0300

    Time: 47 ms (66.00%) | Memory: 23 MB (26.04%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,23 +1,15 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-
-        length =len(people)
-
-        # Step 1: Sort the people into increasing order of there weight.
+        length = len(people)
         people.sort()
 
-        # Step 2: Initialise a Variables for the pointers and to keep count of the boats
-        boats = 0
-        left = 0
-        right = length - 1
+        left, right = 0, length - 1
+        cnt = 0
 
         while left <= right:
-            if (people[left] + people[right]) <= limit:
+            if people[left] + people[right] <= limit:
                 left += 1
-            boats += 1
             right -= 1
-
-        return boats                
-
-        # TC: O(N) 
-        # SC: O(N)
+            cnt += 1
+        return cnt
+            
