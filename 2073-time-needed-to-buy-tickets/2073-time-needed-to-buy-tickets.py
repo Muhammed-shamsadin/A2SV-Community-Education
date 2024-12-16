5563e3e8440afa006f162a83610240135d3ac818
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-12-16 11:25:48 +0300

    Time: 11 ms (41.52%), Space: 17.4 MB (5.85%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -2,14 +2,22 @@ class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         queue = deque()
 
+        # build up the queue
         for i in range(len(tickets)):
-            queue.append([i, tickets[i]])
-        
-        ans = 0
+            queue.append(i)
+
+        result = 0
+
         while queue:
-            ans += 1
-            index, ticket = queue.popleft()
-            if ticket - 1 > 0:
-                queue.append([index, ticket-1])
-            elif index == k:
-                return ans
+            result += 1
+
+            front = queue.popleft()
+            tickets[front] -= 1
+
+            # When our index == k and there is no ticket left anymore
+            if k == front and tickets[front] == 0:
+                return result
+            
+            if tickets[front] != 0:
+                queue.append(front)
+            
